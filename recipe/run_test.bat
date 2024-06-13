fc7c876661177253fb31413b3cd6e772f5e596bf
Lorenzo Pirritano <lpirritano@anaconda.com>
2024-06-13 19:36:42 +0200

    move all tests in run_test

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -8,3 +8,16 @@ if errorlevel 1 exit /B 1
 
 test.exe
 if errorlevel 1 exit /B 1
+
+:: Headers tests - Windows    
+if not exist %PREFIX%\Library\include\wdm.hpp exit 1
+if not exist %PREFIX%\Library\include\wdm\bbeta.hpp exit 1
+if not exist %PREFIX%\Library\include\wdm\eigen.hpp exit 1
+if not exist %PREFIX%\Library\include\wdm\hoeffd.hpp exit 1
+if not exist %PREFIX%\Library\include\wdm\ktau.hpp exit 1
+if not exist %PREFIX%\Library\include\wdm\methods.hpp exit 1
+if not exist %PREFIX%\Library\include\wdm\nan_handling.hpp exit 1
+if not exist %PREFIX%\Library\include\wdm\prho.hpp exit 1
+if not exist %PREFIX%\Library\include\wdm\ranks.hpp exit 1
+if not exist %PREFIX%\Library\include\wdm\srho.hpp exit 1
+if not exist %PREFIX%\Library\include\wdm\utils.hpp exit 1
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -3,6 +3,19 @@ set -x
 # test from:
 # https://github.com/tnagler/wdm/blob/5337860a9a77f147f3adebf4c93fe2ca74d5a028/.github/workflows/main.yml
 
-${CXX} ${CXXFLAGS} ${LDFLAGS} ./test/test.cpp -I$PREFIX/include -L$PREFIX/lib -o test/wdm_test
+${CXX} ${CXXFLAGS} ${LDFLAGS} ./test/test.cpp -I${PREFIX}/include -L${PREFIX}/lib -o test/wdm_test
 
 ./test/wdm_test
+
+# Headers tests - Unix 
+test -e ${PREFIX}/include/wdm.hpp
+test -e ${PREFIX}/include/wdm/bbeta.hpp
+test -e ${PREFIX}/include/wdm/eigen.hpp
+test -e ${PREFIX}/include/wdm/hoeffd.hpp
+test -e ${PREFIX}/include/wdm/ktau.hpp
+test -e ${PREFIX}/include/wdm/methods.hpp
+test -e ${PREFIX}/include/wdm/nan_handling.hpp
+test -e ${PREFIX}/include/wdm/prho.hpp
+test -e ${PREFIX}/include/wdm/ranks.hpp
+test -e ${PREFIX}/include/wdm/srho.hpp
+test -e ${PREFIX}/include/wdm/utils.hpp
